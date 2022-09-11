dab04b1d0fc807be382a2f182430399f24615499
hariharan705 <harih705@gmail.com>
2022-09-11 08:18:43 +0530

    added todays date

--- calculator.py
+++ calculator.py
@@ -4,4 +4,5 @@ print("sum is",first_no+second_no)
 gg="gg"
 print("sub=",first_no-second_no)
 #rd commit
-print("this app in dev for mul")
+print("this app in: dev for mul")
+#hi today date is 9th sep
